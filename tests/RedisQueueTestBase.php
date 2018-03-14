5cf8d2eaba76fac4d2bb58fb7bcf7b6db1df5bf6
wangjian <1636801376@qq.com>
2018-03-14 16:28:34 +0800

    RedisQueue单元测试编写

---
+++ tests/RedisQueueTestBase.php
@@ -0,0 +1,36 @@
+<?php
+namespace Wangjian\Queue\Test;
+
+use PHPUnit\Framework\TestCase;
+use Wangjian\Queue\Job\Commander;
+use Wangjian\Queue\RedisQueue;
+use Predis\Client;
+
+class RedisQueueTestBase extends TestCase
+{
+    protected $client;
+
+    protected $queue;
+
+    protected $commander;
+
+    public function __construct($name = null, array $data = array(), $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->client = new Client([
+            'schema' => 'tcp',
+            'host' => '127.0.0.1',
+            'port' => 6379,
+            'database' => 0
+        ]);
+
+        $this->queue = new RedisQueue($this->client, 'test');
+        $this->commander = new Commander($this->queue);
+    }
+
+    protected function setUp()
+    {
+        $this->client->flushdb();
+    }
+}
