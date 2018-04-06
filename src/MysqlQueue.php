b6808215db083e991e00b4c83c63a3656f468172
wangjian <1636801376@qq.com>
2018-04-06 23:50:37 +0800

    MysqlQueue开发

---
+++ src/MysqlQueue.php
@@ -0,0 +1,78 @@
+<?php
+namespace Wangjian\Queue;
+
+use Wangjian\Queue\Job\AbstractJob;
+use PDO;
+use Exception;
+
+class MysqlQueue implements QueueInterface
+{
+    protected $pdo;
+
+    protected $table;
+
+    public function __construct(PDO $pdo, $table = 'queue')
+    {
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->exec('set names utf8');
+
+        $this->pdo = $pdo;
+        $this->table = $table;
+    }
+
+    public function push(AbstractJob $job)
+    {
+        return $this->addJob($job, $job->getRunAt() > time() ? $job->getRunAt() : time());
+    }
+
+    public function pop($queue)
+    {
+        $sql1 = "SELECT * FROM %s WHERE queue='%s' ORDER BY run_at ASC LIMIT 0, 1 FOR UPDATE";
+        $sql2 = "DELETE FROM %s WHERE ID=%d";
+
+        $this->pdo->beginTransaction();
+        try {
+            $row = $this->pdo->query(sprintf($sql1, $this->table, $queue))->fetch(PDO::FETCH_ASSOC);
+            if(!empty($row)) {
+                $this->pdo->exec(sprintf($sql2, $this->table, $row['id']));
+            }
+
+            $this->pdo->commit();
+        } catch (Exception $e) {
+            $this->pdo->rollBack();
+
+            throw $e;
+        }
+
+        if(!empty($row)) {
+            $job = @unserialize($row['job']);
+            if ($job instanceof AbstractJob) {
+                return $job;
+            }
+        }
+
+        return null;
+    }
+
+    public function retry(AbstractJob $job)
+    {
+        if ($job->shouldRetry()) {
+            return $this->addJob($job, $job->getRetryTime());
+        }
+
+        return false;
+    }
+
+    protected function addJob(AbstractJob $job, $runAt)
+    {
+        $sql = "INSERT INTO %s(queue, job, run_at) VALUES('%s', '%s', %d)";
+
+        return (bool)$this->pdo->exec(sprintf(
+            $sql,
+            $this->table,
+            $job->getQueue(),
+            addslashes(serialize($job)),
+            $runAt
+        ));
+    }
+}
--- src/Traits/CommandConfigTrait.php
+++ src/Traits/CommandConfigTrait.php
@@ -31,11 +31,11 @@ trait CommandConfigTrait
                     $this->configVariables = $configs;
                 } else {
                     $output->writeln('<info>the config file format is incorrect...</info>');
-                    exit;
+                    exit(1);
                 }
             } else {
                 $output->writeln('<info>the config file does not exist...</info>');
-                exit;
+                exit(1);
             }
         }
 
@@ -43,7 +43,7 @@ trait CommandConfigTrait
         foreach($input->getOption('environment') as $env) {
             if(!$this->checkEnvironmentVariable($env)) {
                 $output->writeln('<info>the environment variables are not incorrect...</info>');
-                exit;
+                exit(1);
             }
 
             list($key, $value) = explode('=', $env, 2);
